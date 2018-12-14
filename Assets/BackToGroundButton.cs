999cc3fd8e3fda76ceb2750efbc6ddabf114231f
khl2bb <43461931+khl2bb@users.noreply.github.com>
2018-12-14 16:01:04 +0900

    Add files via upload

---
+++ Assets/BackToGroundButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI; //Allows us to use UI.
+
+public class BackToGroundButton : MonoBehaviour
+{
+
+    public GameObject MessageLobbyImage;
+    public GameObject GoToFirstChoiceButton;
+    public GameObject PhoneGroundImage;
+    public GameObject GoToMessageLobbyButton;
+    
+
+    public void OnClickStart()
+    {
+
+        MessageLobbyImage.SetActive(false);
+        GoToFirstChoiceButton.SetActive(false);
+        PhoneGroundImage.SetActive(true);
+        GoToMessageLobbyButton.SetActive(true);
+        Debug.Log("Go to MessageLobby Button Clicked");
+    }
+}
--- Assets/MessageButtonScript.cs
+++ Assets/MessageButtonScript.cs
@@ -9,12 +9,18 @@ public class MessageButtonScript : MonoBehaviour
 
     public GameObject MessageLobbyImage;
     public GameObject MessageToButton;
+    public GameObject MessageFirstChoice;
+    public GameObject GoToSNGButton;
+    public GameObject NotGoingSNGButton;
 
     public void OnClickStart()
     {
 
         MessageLobbyImage.SetActive(true);
         MessageToButton.SetActive(true);
-        Debug.Log("Message Button Clicked");
+        MessageFirstChoice.SetActive(false);
+        GoToSNGButton.SetActive(false);
+        NotGoingSNGButton.SetActive(false);
+        Debug.Log("Go to MessageLobby Button Clicked");
     }
 }
